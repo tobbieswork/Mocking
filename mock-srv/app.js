61eb38bb7ede49f87f0d5cb60ac77141bda8cf9c
Tobbies <dangphat.job@gmail.com>
2024-09-17 22:06:11 +0700

    Initial files for mocking api with nodejs and fastify

---
+++ mock-srv/app.js
@@ -0,0 +1,32 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import AutoLoad from '@fastify/autoload'
+import cors from '@fastify/cors'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+// Pass --options via CLI arguments in command to enable these options.
+export const options = {}
+
+export default async function (fastify, opts) {
+  // Place here your custom code!
+  fastify.register(cors)
+  // Do not touch the following lines
+
+  // This loads all plugins defined in plugins
+  // those should be support plugins that are reused
+  // through your application
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'plugins'),
+    options: Object.assign({}, opts)
+  })
+
+  // This loads all plugins defined in routes
+  // define your routes in one of these
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'routes'),
+    options: Object.assign({}, opts)
+  })
+}
+
